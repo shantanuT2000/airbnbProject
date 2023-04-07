78f92a0247e268f644e33eb699bb08d917a15b1f
Shantanu Tidke <shantanutidke2000@gmail.com>
2023-04-07 17:50:17 +0530

    Added register form with validation

--- src/components/HotelManagementApp.js
+++ src/components/HotelManagementApp.js
@@ -1,28 +1,24 @@
-import React,{useState} from 'react'
+import React from 'react'
 import HotelManagement from './HotelManagement';
-import {Routes,Route} from 'react-router-dom';
-import Signup from "./Signup";
-import Login from "./Login";
 import NavBar from './NavBar';
 import Footer from './Footer';
 
+
 const HotelManagementApp = () => {
   const navItems=[{id:1,path:"/signup",item:"Register"},{id:2,path:"/login",item:"Login"}];
-  const[Navi,setNav] = useState(navItems);
+  
   return (
-    <div className="">
-    <NavBar Navi={Navi}/>
-    <Routes>
-            <Route exact path='/' element={<HotelManagement/>}></Route>
-            <Route path='/signup' element={<Signup/>}></Route>
-            <Route path='/login' element={<Login/>}></Route>
-
-    </Routes>
+      
+      <div className="">
+    <NavBar Navi={navItems}/>
+    <HotelManagement/>
     <hr />
    <Footer/>
     
      
     </div>
+   
+
   );
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,23 @@
 import React from 'react'
-
+import {useForm} from 'react-hook-form'
+import './css/Hotel.css'
 const Login = () => {
+  const {register,handleSubmit} = useForm();
+  const onSubmit = (data) =>{
+  console.log(data);
+  }
   return (
-    <div>Login</div>
+    <>
+    
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="text" placeholder='Name' {...register("fullName")}/>
+      <input type="text" placeholder='Email' {...register('email')}/>
+      <input type="number" placeholder='Age' {...register('age')}/>
+      <input type="password" placeholder='password' {...register('password')}/>
+      <input type="password" placeholder='confirm password'{...register('confirmPassword')} />
+      <input type="submit" value='submit' />
+    </form>
+    </>
   )
 }
 
